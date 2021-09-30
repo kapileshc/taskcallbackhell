9f449a4eac8ca5fdebad39e4cd045faff1a1cd53
kapileshc <85663945+kapileshc@users.noreply.github.com>
2021-09-30 10:08:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,72 @@
+setTimeout(()=>{
+
+    var h1 = document.getElementById("counter");
+    h1.innerHTML ="10";
+     setTimeout(()=>
+     {
+        var h1 = document.getElementById("counter");
+        h1.innerHTML ="9";
+         setTimeout(()=>
+         {
+            var h1 = document.getElementById("counter");
+            h1.innerHTML ="8";
+             setTimeout(()=>
+             {
+                var h1 = document.getElementById("counter");
+                h1.innerHTML ="7";
+                     setTimeout(()=>
+                     {
+                    var h1 = document.getElementById("counter");
+                    h1.innerHTML ="6";
+                     setTimeout(()=>
+                     {
+                        var h1 = document.getElementById("counter");
+                        h1.innerHTML ="5";
+         
+                         setTimeout(()=>
+                         {
+                            var h1 = document.getElementById("counter");
+                            h1.innerHTML ="4";
+                             setTimeout(()=>
+                             {
+                                var h1 = document.getElementById("counter");
+                                h1.innerHTML ="3";
+                                 setTimeout(()=>
+                                 {
+                                    var h1 = document.getElementById("counter");
+                                    h1.innerHTML ="2"; 
+                                     setTimeout(()=>
+                                     {
+                                        var h1 = document.getElementById("counter");
+                                        h1.innerHTML ="1";
+                                         setTimeout(()=>
+                                         {
+                                            var h1 = document.getElementById("counter");
+                                            h1.innerHTML ="Happy Independence Day";  
+                                            var image=document.createElement("img");
+                                            image.setAttribute("src","https://static.toiimg.com/photo/msid-77549543/77549543.jpg?80271")
+                                            var display=document.getElementById("image")
+                                            display.appendChild(image)
+                                        },1000);
+                                    
+                                    },1000);                                          
+                                
+                                },1000);                                         
+                            
+                            },1000);                                          
+                        
+                        },1000);
+                                                                  
+                    
+                    },1000);                                           
+                
+                },1000);   
+            
+            },1000);   
+        
+        },1000);  
+    
+    },1000);
+
+
+},1000);
